964f4f2bb3cd2d3547659fea8ec883d4e3851215
AisenFei <1374873433@qq.com>
2020-12-10 21:25:45 +0800

    礼让线程

---
+++ 多线程/TestYield.java
@@ -0,0 +1,19 @@
+package Thread;
+
+//测试礼让线程
+class MyYield implements Runnable{
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName()+"-->start");
+        Thread.yield();//礼让
+        System.out.println(Thread.currentThread().getName()+"-->end");
+    }
+}
+public class TestYield {
+    public static void main(String[] args) {
+        MyYield myYield = new MyYield();
+        new Thread(myYield,"a").start();
+        new Thread(myYield,"b").start();
+        new Thread(myYield,"c").start();
+    }
+}
