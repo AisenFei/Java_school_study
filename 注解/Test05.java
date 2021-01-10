ec4594f8d952911b259180b8dff171dbaa845861
AisenFei <1374873433@qq.com>
2021-01-10 22:54:13 +0800

    什么是反射

---
+++ 注解/Test05.java
@@ -0,0 +1,68 @@
+package code_2021_0110;
+
+//什么叫反射
+public class Test05 {
+    public static void main(String[] args) throws ClassNotFoundException {
+        //通过反射获取类的Class对象
+        Class c1 = Class.forName("code_2021_0110.User");
+        System.out.println(c1);
+
+        Class c2 = Class.forName("code_2021_0110.User");
+        Class c3 = Class.forName("code_2021_0110.User");
+        Class c4 = Class.forName("code_2021_0110.User");
+
+        //一个类在内存中只有一个Class对象
+        //一个类被加载之后，类的整个结构都会被封装在Class对象中
+        System.out.println(c2);
+        System.out.println(c3);
+        System.out.println(c4);
+    }
+}
+
+//实体类：pojo，entity
+class User{
+    private String name;
+    private int id;
+    private int age;
+
+    public User(){}
+
+    public User(String name,int id,int age){
+        this.name = name;
+        this.id = id;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", age=" + age +
+                '}';
+    }
+}
